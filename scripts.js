7913231b2d4d65b80c3c3c0876abbf6cc419cf86
harsal-patel <harsalanilpatel@gmail.com>
2022-06-10 15:21:25 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,94 @@
+const patients = [
+    {
+        "id": "jdoe061278",
+        "password": "password1",
+        "firstname": "John",
+        "lastname": "Doe",
+        "birthdate": "06/12/1978",
+        "street": "153 N Pine St",
+        "citystatezip": "Los Angeles, CA 91775",
+        "phone": "626-493-5838",
+        "height": "5'11",
+        "weight": "187",
+        "sex": "male",
+        "medication": [],
+        "visitdate": "4/03/2022",
+        "doctor": "Dr. Jason Merritt",
+        "visitnotes": "Blood pressure: 132/84. Pulse 74 bpm. Patient came in for routine checkup. Blood pressure is marginally lower compared to previous visit. This is a sign that the medication has been helping in regards to patient's high blood pressure. Blood pressure is still higher than would like. Patient is advised to return for another checkup in 6 months to make sure medication is still lowering blood pressure. Results of lab report show higher than normal cholesterol and blood sugar levels. Patient is advised to alter diet to reduce these levels. Rest of report is normal. Medication dosages unchanged.",
+        "labtestdate": "03/23/2022",
+        "nextvisit": "10/12/2022"
+    },
+    {
+        "id": "mjohnson112486",
+        "password": "password2",
+        "firstname": "Mary",
+        "lastname": "Johnson",
+        "birthdate": "11/24/1986",
+        "street": "972 Prospect St",
+        "citystatezip": "Camden, NJ 08102",
+        "phone": "856-703-5519",
+        "height": "5'3",
+        "weight": "142",
+        "sex": "female",
+        "medication": [],
+        "visitdate": "2/24/22",
+        "doctor": "Dr. Jason Merritt",
+        "visitnotes": "Blood pressure: 120/75. Pulse 68 bpm. Patient came in with cold symptoms, fever, and irritation in back of throat. Further analysis showed swollen lymph nodes in the neck. Conclusions is that patient has strep throat. Prescribed antibiotics to be taken over a 5 day period. Patient is advised to check back in with clinic if symptoms do not improve over the next 48 hours.",
+        "labtestdate": "12/18/21",
+        "nextvisit": "10/04/22"
+    }
+]
+
+let index = sessionStorage.getItem("index");
+let patientFound = sessionStorage.getItem("patientFound");
+let physicianFound = sessionStorage.getItem("physicianFound");
+
+function validatePatient(form) {
+    let username = document.getElementById("patientID").value;
+    let password = document.getElementById("patientPassword").value;
+    patientFound = false;
+    for (let i = 0; i < patients.length; i++) {
+        if (username === patients[i].id && password === patients[i].password) {
+            patientFound = true;
+            sessionStorage.setItem("patientFound", patientFound);
+            sessionStorage.setItem("index", i);
+        }
+    }
+    console.log(patientFound);
+    if (!patientFound) {
+        alert("Incorrect username and/or password!");
+    }
+    else {
+        form.action = "patientview.html";
+        return true;
+    }
+}
+
+function validatePhysician(form) {
+    let username = document.getElementById("physicianID").value;
+    let password = document.getElementById("physicianPassword").value;
+    if (username === "jmerritt76" && password === "password0") {
+        sessionStorage.setItem("physicianFound", true);
+        form.action = "physicianview.html";
+        return true;
+    }
+    else {
+        alert("Incorrect username and/or password!");
+    }
+}
+
+if (patientFound) {
+    document.getElementById('patientid').innerHTML = patients[index].id;
+    document.getElementById('patientname').innerHTML = `${patients[index].firstname} ${patients[index].lastname}`;
+    document.getElementById('patientbirthdate').innerHTML = patients[index].birthdate;
+    document.getElementById('patientaddress').innerHTML = `${patients[index].street}, ${patients[index].citystatezip}`;
+    document.getElementById('patientphone').innerHTML = patients[index].phone;
+    document.getElementById('patientsex').innerHTML = patients[index].sex;
+    document.getElementById('patientheight').innerHTML = patients[index].height;
+    document.getElementById('patientweight').innerHTML = patients[index].weight;
+    document.getElementById('patientlabdate').innerHTML = patients[index].labtestdate;
+    document.getElementById('patientvisitdate').innerHTML = patients[index].visitdate;
+    document.getElementById('patientnextdate').innerHTML = patients[index].nextvisit;
+    document.getElementById('patientdoctor').innerHTML = patients[index].doctor;
+    document.getElementById('patientnotes').innerHTML = patients[index].visitnotes;
+}
